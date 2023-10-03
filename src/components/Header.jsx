53f16a91b4785825ac16d0f844000834b54e19c7
Elgun837 <elgun.xalilov@bk.ru>
2023-10-03 17:47:12 +0400

    router error fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,15 +10,6 @@ const Header = () => {
             <li>
               <Link to="/homepage">Homepage</Link>
             </li>
-            <li>
-              <a href="/about">About us</a>
-            </li>
-            <li>
-              <a href="/services">Services</a>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
           </ul>
         </div>
       </nav>
